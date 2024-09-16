6e02cf36f523ecf0bda4c37fb033799f0e73337b
mollyk <katsouli.mo@gmail.com>
2024-09-16 18:29:47 +0300

    Creates navbar html css

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,15 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  isLoggedIn = false;  // Simulate login state
+  userPicture = 'assets/user-avatar.jpg';  // Placeholder user picture
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
 
 }
